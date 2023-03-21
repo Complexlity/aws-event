35787c05be48d30c5278954d17d4dddfdfe2678f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-03-21 21:05:56 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+     screens: {
+      sm: "480px",
+      md: "768px",
+      mg: "976px",
+      xl: "1440px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
